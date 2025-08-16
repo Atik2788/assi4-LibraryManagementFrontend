7f4819403d40a60a12459bba6c15756b5c609065
Atik2788 <ashanto653.2@gmail.com>
2025-08-16 18:08:15 +0600

    edit some build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import * as path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+// Node.js style __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // এখন ঠিক হবে
     },
   },
-})
+});
